eaceda0dd90a30633f79ce9d242e05e0acbecc7c
Raxoria <76510278+Raxoria@users.noreply.github.com>
2021-04-05 18:58:27 +0700

    Add files via upload

---
+++ Listing 3.1/src/FirstSample.java
@@ -0,0 +1,13 @@
+/**
+ *This is the first program Java core
+ *@version 1.01 15 March 2020
+ *@author Syelvie Ira
+ */
+
+public class FirstSample
+{
+ public static void main(String[] args)
+ {
+   System.out.println("We will not use 'Hello World again:)'");
+ }
+}
