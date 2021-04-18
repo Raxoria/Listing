a2282229ae50bc5aea18892f985f1d2261f1e2da
Raxoria <76510278+Raxoria@users.noreply.github.com>
2021-04-18 15:14:42 +0700

    Add files via upload

---
+++ Naruto/src/naruto/NarutoAnime.java
@@ -0,0 +1,10 @@
+package naruto;
+
+public class NarutoAnime {
+	public static void main(String[] args) {
+		sasuke s = new sasuke();
+			s.printDojutsu();
+		Itachi i = new sasuke();
+			i.printKekkeiGenkai();
+	}
+}
---
+++ Naruto/src/naruto/sasuke.java
@@ -0,0 +1,9 @@
+package naruto;
+
+public class sasuke extends Itachi {
+	String Dojutsu = "Sharingan";
+		void printDojutsu() {
+			super.printDojutsu();
+			System.out.println(this.Dojutsu);
+	}
+}
